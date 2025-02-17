ae0d2bfb73f71a005654486ea1b51421fab9108e
nengwi <nengmilaningrum@gmail.com>
2025-02-17 10:27:14 +0700

    FLASH MESSAGE  bertujuan agar alert didalam sebuah action berhasil tampil

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -27,13 +28,24 @@ class HandleInertiaRequests extends Middleware
      *
      * @return array<string, mixed>
      */
-    public function share(Request $request): array
+    public function share(Request $request): array // Ini adalah deklarasi fungsi share(). Fungsi ini menerima parameter $request yang merupakan permintaan HTTP saat ini dan mengembalikan sebuah array.//
     {
-        return [
-            ...parent::share($request),
-            'auth' => [
+        return array_merge(parent::share($request), [ //Fungsi ini menggabungkan data yang akan dibagikan dengan tampilan. parent::share($request) memanggil implementasi share() dari kelas induk (parent class), dan kemudian hasilnya digabungkan dengan data tambahan yang akan ditambahkan dalam array asosiatif.//
+            'auth' => [ //Bagian ini menambahkan data yang terkait dengan otentikasi pengguna. Ini mencakup informasi pengguna saat ini yang diambil dari permintaan HTTP.//
                 'user' => $request->user(),
             ],
-        ];
+            'ziggy' => function () use ($request) {  // Bagian ini menambahkan data yang terkait dengan routing JavaScript yang dihasilkan oleh Ziggy, sebuah pustaka yang membantu dalam menangani routing pada aplikasi web JavaScript. Ini termasuk URL saat ini yang diambil dari permintaan HTTP.//   
+                return array_merge((new Ziggy)->toArray(), [
+                    'location' => $request->url(),
+                ]);
+            },
+            'flash' => function () use ($request) { // Bagian ini menambahkan data yang terkait dengan pesan flash. Pesan flash biasanya digunakan untuk menampilkan pesan sukses atau pesan kesalahan kepada pengguna setelah tindakan tertentu. Data ini diambil dari sesi (session) dan termasuk pesan sukses dan pesan kesalahan yang mungkin ada.//
+                return [
+                    'success' => $request->session()->get('success'),
+                    'error' => $request->session()->get('error'),
+                ];
+            },
+        ]);
     }
 }
+
