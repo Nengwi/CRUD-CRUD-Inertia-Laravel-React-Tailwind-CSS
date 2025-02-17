6ada79d1e49c2d25c8c32c57133d8ad3745df92c
nengwi <nengmilaningrum@gmail.com>
2025-02-17 09:45:34 +0700

    melakukan konfigurasi pada model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace App\Models;
+namespace App\Models; // Ini adalah deklarasi namespace yang menentukan lokasi kelas //
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; //alat bantu yang digunakan untuk mempermudah pembuatan data dummy (dummy data) saat mengembangkan dan menguji aplikasi.//
+use Illuminate\Database\Eloquent\Model; //Ini adalah pernyataan use yang mengimpor kelas Model dari Laravel.//
 
-class Post extends Model
+class Post extends Model // Ini adalah deklarasi kelas Post yang menggambarkan model Eloquent dalam aplikasi Laravel.//
 {
-    use HasFactory;
+    use HasFactory; // Ini adalah penggunaan trait HasFactory. Trait ini memberikan kemampuan untuk membuat data dummy yang terkait dengan model Post.//
+
+    protected $fillable = [
+        'title', 'body'     
+    ]; //Ini adalah properti yang mendefinisikan kolom-kolom dalam tabel yang dapat diisi (fillable) secara massal (mass-assignment).//
 }
+
