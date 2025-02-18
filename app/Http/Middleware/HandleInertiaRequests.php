4a40e1e4eef2dcbb17f8c06c5c8110cf79d39ceb
nengwi <nengmilaningrum@gmail.com>
2025-02-18 09:30:15 +0700

    Paket ini memerlukan akses ke pengaturan konfigurasi izin untuk menjalankan migrasi

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
-
+use Illuminate\Support\Facades\Auth; 
 class HandleInertiaRequests extends Middleware
 {
     /**
@@ -28,18 +28,30 @@ class HandleInertiaRequests extends Middleware
      *
      * @return array<string, mixed>
      */
-    public function share(Request $request): array // Ini adalah deklarasi fungsi share(). Fungsi ini menerima parameter $request yang merupakan permintaan HTTP saat ini dan mengembalikan sebuah array.//
+    public function share(Request $request): array
     {
-        return array_merge(parent::share($request), [ //Fungsi ini menggabungkan data yang akan dibagikan dengan tampilan. parent::share($request) memanggil implementasi share() dari kelas induk (parent class), dan kemudian hasilnya digabungkan dengan data tambahan yang akan ditambahkan dalam array asosiatif.//
-            'auth' => [ //Bagian ini menambahkan data yang terkait dengan otentikasi pengguna. Ini mencakup informasi pengguna saat ini yang diambil dari permintaan HTTP.//
+        $user = Auth::user();
+
+        if ($user) {
+            $user->load('roles');
+
+            $user = [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'roles' => $user->roles,
+            ];
+        }
+        return array_merge(parent::share($request), [
+            'auth' => [
                 'user' => $request->user(),
             ],
-            'ziggy' => function () use ($request) {  // Bagian ini menambahkan data yang terkait dengan routing JavaScript yang dihasilkan oleh Ziggy, sebuah pustaka yang membantu dalam menangani routing pada aplikasi web JavaScript. Ini termasuk URL saat ini yang diambil dari permintaan HTTP.//   
+            'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
                     'location' => $request->url(),
                 ]);
             },
-            'flash' => function () use ($request) { // Bagian ini menambahkan data yang terkait dengan pesan flash. Pesan flash biasanya digunakan untuk menampilkan pesan sukses atau pesan kesalahan kepada pengguna setelah tindakan tertentu. Data ini diambil dari sesi (session) dan termasuk pesan sukses dan pesan kesalahan yang mungkin ada.//
+            'flash' => function () use ($request) {
                 return [
                     'success' => $request->session()->get('success'),
                     'error' => $request->session()->get('error'),
